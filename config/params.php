d0ce1b26798c6bac355bfa75e780d9d7962622e0
Maksimov Roman <maksimov.ro.al@gmail.com>
2020-03-12 21:01:56 +0300

    добавил тестовые аутентификационные данные для API PickPoint

--- config/params.php
+++ config/params.php
@@ -4,4 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'pickpoint' => require __DIR__ . '/pickpoint.php',
 ];
