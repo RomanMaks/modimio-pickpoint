bbf4f1521b6b7ae0fd6212a3d845b6c751c977e0
Maksimov Roman <maksimov.ro.al@gmail.com>
2020-03-19 21:56:03 +0300

    Добавил пустышку для тестирования реестра

---
+++ tests/unit/models/RegistryTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace unit\models;
+
+use Codeception\Test\Unit;
+
+/**
+ * Unit тестирование реестра
+ *
+ * Class RegistryTest
+ * @package unit\models
+ */
+class RegistryTest extends Unit
+{
+    public function testRecreateRegistry()
+    {
+
+    }
+}
